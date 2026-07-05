2a376c7fae00e6d4d5ce489c46cfc6c7b0729637
Maxhead200 <maxhead2000@gmail.com>
2026-07-05 23:51:36 +0300

    Restore TSD menu with employee login

--- wms/apps/android-tsd/app/build.gradle.kts
+++ wms/apps/android-tsd/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "pro.logoff.wms.tsd"
         minSdk = 26
         targetSdk = 35
-        versionCode = 38
-        versionName = "0.1.34"
+        versionCode = 39
+        versionName = "0.1.39"
     }
 
     compileOptions {
